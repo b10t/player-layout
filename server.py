72c1ef1493d13a2cbcfa1b9f33c07343ad5e94ff
b10t <b10t.rus@gmail.com>
2022-11-30 12:56:46 +0700

    Add play button.

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from livereload import Server
 
 def main():
     server = Server()
-    server.watch('yandex.html')
-    server.serve(root='.', default_filename='./yandex.html')
+    server.watch('index.html')
+    server.serve(root='.', default_filename='./index.html')
 
 
 if __name__ == '__main__':
