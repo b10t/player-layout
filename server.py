615ac7d00a3d84ec0be45e564bd8ca086b1894b0
b10t <b10t.rus@gmail.com>
2022-11-30 12:52:38 +0700

    Add yandex page.

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from livereload import Server
 
 def main():
     server = Server()
-    server.watch('index.html')
-    server.serve(root='.', default_filename='./index.html')
+    server.watch('yandex.html')
+    server.serve(root='.', default_filename='./yandex.html')
 
 
 if __name__ == '__main__':
